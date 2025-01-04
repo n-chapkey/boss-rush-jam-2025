e35a21a9beb54dcabe01e6a994d9830acc19d6ad
haladrias <haladrias@gmail.com>
2025-01-04 15:18:27 -0800

    Added loading screen scene, began player UI, added camera2d to player

--- main/scene's/player.gd
+++ main/scene's/player.gd
@@ -1,11 +1,13 @@
 extends CharacterBody2D
 
-@onready var hitbox = $CollisionShape2D
+@onready var hitbox_player = $CollisionShape2D
 var dir:String = "none"
 var is_moving:bool = false
 @onready var anim = $AnimatedSprite2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+@export var player_damage_attack : int = 10
+@export var player_health_max : int = 100
 
 
 func _physics_process(delta: float) -> void:
@@ -27,7 +29,7 @@ func _physics_process(delta: float) -> void:
 			anim.play("run")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		if velocity.y == 0:
+		if velocity.y == 0 and velocity.x == 0:
 			anim.play("idle")
 	
 	if direction == -1:
@@ -36,17 +38,27 @@ func _physics_process(delta: float) -> void:
 	elif direction == 1:
 		anim.flip_h = false
 		
-	if Input.is_action_pressed("sit") and is_on_floor():
+	if Input.is_action_pressed("sit") and velocity.y == 0:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 		anim.play("crouch")
+	
+	elif Input.is_action_pressed("player_attack"):
+		anim.play("attack")
+		
 		
 	
 	
 	
 	move_and_slide()
-	
+
 
 
 func _on_area_2d_3_body_entered(body: CharacterBody2D) -> void:
 	position.x = 2589
 	position.y = 706
+
+func player_attack() -> void:
+	pass
+
+	
